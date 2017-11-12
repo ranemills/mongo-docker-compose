1b214a423ef7b0b6a095f468ad4622e7b4064b6e
Ryan Mills <ranemills@gmail.com>
2017-11-12 23:18:08 +0000

    Collapse all servers into two - one outward and one which can be scaled up - and fix automation agent

--- mongo-with-automation/startup.sh
+++ mongo-with-automation/startup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-nohup /opt/mongodb-mms-automation-agent/mongodb-mms-automation-agent >> automation-agent.log 2>&1 &
-tail -f automation-agent.log
+nohup /opt/mongodb-mms-automation-agent/mongodb-mms-automation-agent -f /opt/mongodb-mms-automation-agent/local.config >> automation-agent.log 2>&1 &
+tail -f /var/log/mongodb-mms-automation/automation-agent.log
