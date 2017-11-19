75d07230373ff9e28389895a29129e329ea79fe5
Ryan Mills <ranemills@gmail.com>
2017-11-19 13:25:39 +0000

    Call mongodb-mms-automation-agent directly

--- mongo-with-automation/startup.sh
+++ mongo-with-automation/startup.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-nohup /opt/mongodb-mms-automation-agent/mongodb-mms-automation-agent -f /opt/mongodb-mms-automation-agent/local.config >> automation-agent.log 2>&1 &
-tail -f /var/log/mongodb-mms-automation/automation-agent.log
+/opt/mongodb-mms-automation-agent/mongodb-mms-automation-agent -f /opt/mongodb-mms-automation-agent/local.config
